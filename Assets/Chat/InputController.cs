470b7b74ba96feb3357980ee6eff0c80168436e5
asachun <chuchuchu.chunchun@gmail.com>
2016-07-16 22:49:38 +0900

    Add InputController.

---
+++ Assets/Chat/InputController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InputController : MonoBehaviour
+{
+	[SerializeField] InputField m_InputField;
+
+	void Awake ()
+	{
+
+	}
+
+	public void OnClick ()
+	{
+		m_InputField.text = "";
+	}
+
+	#if UNITY_EDITOR
+
+	void Reset ()
+	{
+		m_InputField = FindObjectOfType<InputField> ();
+	}
+
+	#endif
+}
