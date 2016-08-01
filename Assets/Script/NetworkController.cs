e13cd614b28a38ea4aa63a7b122a13287f556fb6
asachun <chuchuchu.chunchun@gmail.com>
2016-08-01 17:11:25 +0900

    Add NetworkController and SocketObserver.

---
+++ Assets/Script/NetworkController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+﻿using UnityEngine;
+
+
+public class NetworkController : MonoBehaviour
+{
+	[SerializeField] SocketObserver m_SocketObserver;
+
+	#if UNITY_EDITOR
+
+	void Reset()
+	{
+		m_SocketObserver = GetComponent<SocketObserver>();
+	}
+
+	#endif
+}
