d070205b5d73c649a64e1e41b3dc9c4271082f66
asachun <chuchuchu.chunchun@gmail.com>
2016-08-01 17:50:57 +0900

    Add GazePointer.

---
+++ Assets/Script/GazePointer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+
+public class GazePointer : MonoBehaviour, IGvrGazePointer
+{
+	void OnEnable()
+	{
+		GazeInputModule.gazePointer = this;
+	}
+
+	void OnDisable()
+	{
+		if (GazeInputModule.gazePointer == (IGvrGazePointer)this)
+		{
+			GazeInputModule.gazePointer = null;
+		}
+	}
+
+	#region Implement IGvrGazePointer.
+
+	public void OnGazeEnabled() {}
+
+	public void OnGazeDisabled() {}
+
+	public void OnGazeStart(Camera camera, GameObject targetObject, Vector3 intersectionPosition, bool isInteractive) {}
+
+	public void OnGazeStay(Camera camera, GameObject targetObject, Vector3 intersectionPosition, bool isInteractive) {}
+
+	public void OnGazeExit(Camera camera, GameObject targetObject) {}
+
+	public void OnGazeTriggerStart(Camera camera) {}
+
+	public void OnGazeTriggerEnd(Camera camera) {}
+
+	public void GetPointerRadius(out float innerRadius, out float outerRadius)
+	{
+		innerRadius = 0.0f;
+		outerRadius = 0.1f;
+	}
+
+	#endregion
+
+	#if UNITY_EDITOR
+
+	void Reset()
+	{
+
+	}
+
+	#endif
+}
