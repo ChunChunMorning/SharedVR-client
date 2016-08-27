3e8a77663d40c556c10d7755e003e49110353395
asachun <chuchuchu.chunchun@gmail.com>
2016-08-27 20:03:38 +0900

    Implement SettingManager::DisplayFailure.

--- Assets/Script/Setting/SettingManager.cs
+++ Assets/Script/Setting/SettingManager.cs
@@ -8,8 +8,10 @@ public class SettingManager : MonoBehaviour
 	[SerializeField] Toggle m_VRMode;
 	[SerializeField] InputField m_IPAddress;
 	[SerializeField] InputField m_PortNumber;
+	[SerializeField] Text m_Message;
 
 	NetworkController m_NetworkController;
+	Coroutine m_DisplayFailureCoroutine;
 
 	void Start()
 	{
@@ -31,7 +33,19 @@ public class SettingManager : MonoBehaviour
 
 	private void OnFailure()
 	{
-		Debug.Log("Failure");
+		if (m_DisplayFailureCoroutine != null)
+			StopCoroutine(m_DisplayFailureCoroutine);
+		
+		m_DisplayFailureCoroutine = StartCoroutine(DisplayFailure());
+	}
+
+	private IEnumerator DisplayFailure()
+	{
+		m_Message.text = "Sorry, I can't connect.";
+
+		yield return new WaitForSeconds(1.0f);
+
+		m_Message.text = "Please retry.";
 	}
 
 	#if UNITY_EDITOR
@@ -41,6 +55,7 @@ public class SettingManager : MonoBehaviour
 		m_VRMode = GameObject.Find("VRMode").GetComponent<Toggle>();
 		m_IPAddress = GameObject.Find("IPAddress/InputField").GetComponent<InputField>();
 		m_PortNumber = GameObject.Find("Port/InputField").GetComponent<InputField>();
+		m_Message = GameObject.Find("Message").GetComponent<Text>();
 	}
 
 	#endif
