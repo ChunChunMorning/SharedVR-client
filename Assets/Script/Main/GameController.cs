51be248a17c2b6bcf7f055eead733b37ed849b28
asachun <chuchuchu.chunchun@gmail.com>
2016-08-27 21:27:25 +0900

    Add GameController.

---
+++ Assets/Script/Main/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+	void Awake()
+	{
+#if UNITY_EDITOR
+		// If scene is already loaded.
+		if (SceneManager.sceneCount > 1)
+			return;
+#endif
+		SceneManager.LoadScene("Setting", LoadSceneMode.Additive);
+	}
+}
