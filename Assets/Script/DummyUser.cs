4d231d6f94329cf56baa09277aacb04fb2dd0d55
asachun <chuchuchu.chunchun@gmail.com>
2016-09-08 01:12:40 +0900

    Add DummyUser.

---
+++ Assets/Script/DummyUser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+﻿using UnityEngine;
+
+public class DummyUser : MonoBehaviour
+{
+	void Awake()
+	{
+
+	}
+
+	void Update()
+	{
+		
+	}
+
+#if UNITY_EDITOR
+	void Reset()
+	{
+
+	}
+#endif
+}
