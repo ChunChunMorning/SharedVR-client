049bcef53077ccc3828bd1e8bb62daffaf98d44d
asachun <chuchuchu.chunchun@gmail.com>
2016-10-03 14:55:05 +0900

    Implement Forest Scene.

--- Assets/Script/Setting/SettingManager.cs
+++ Assets/Script/Setting/SettingManager.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SettingManager : MonoBehaviour
 {
-	[SerializeField] Toggle m_VRMode;
-	[SerializeField] InputField m_IPAddress;
-	[SerializeField] InputField m_PortNumber;
-	[SerializeField] Text m_Message;
+	[SerializeField]
+	private string m_NextScene;
 
-	Coroutine m_DisplayFailureCoroutine;
+	[SerializeField]
+	private Toggle m_VRMode;
+
+	[SerializeField]
+	private InputField m_IPAddress;
+
+	[SerializeField]
+	private InputField m_PortNumber;
+
+	[SerializeField]
+	private Text m_Message;
+
+	private Coroutine m_DisplayFailureCoroutine;
 
 	public void OnStartClicked()
 	{
@@ -27,7 +36,7 @@ public class SettingManager : MonoBehaviour
 
 		FindObjectOfType<GazeInputModule>().enabled = true;
 		FindObjectOfType<StandaloneInputModule>().enabled = false;
-		FindObjectOfType<GameController>().SwitchScene("Setting", "Primitive", 0.5f);
+		FindObjectOfType<GameController>().SwitchScene("Setting", m_NextScene, 0.5f);
 	}
 
 	private void OnFailure()
@@ -42,13 +51,12 @@ public class SettingManager : MonoBehaviour
 	{
 		m_Message.text = "Sorry, I can't connect.";
 
-		yield return new WaitForSeconds(1.0f);
+		yield return new WaitForSeconds(1f);
 
 		m_Message.text = "Please retry.";
 	}
 
-	#if UNITY_EDITOR
-
+#if UNITY_EDITOR
 	void Reset()
 	{
 		m_VRMode = GameObject.Find("VRMode").GetComponent<Toggle>();
@@ -56,6 +64,5 @@ public class SettingManager : MonoBehaviour
 		m_PortNumber = GameObject.Find("Port/InputField").GetComponent<InputField>();
 		m_Message = GameObject.Find("Message").GetComponent<Text>();
 	}
-
-	#endif
+#endif
 }
